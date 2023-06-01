d2317a65d5093f2afba4ad58386be760a0c7e52e
Jongpil Kim <jpkim@koceti.re.kr>
2023-06-01 21:29:43 +0900

    feat[teleop_key]: PyQt 키보드입력 라이브러리를 사용하도록 변경

--- roller_control/teleop_key.py
+++ roller_control/teleop_key.py
@@ -1,42 +1,54 @@
+from PyQt5 import QtCore, QtGui
 import rclpy
 from rclpy.node import Node
 
+import sys
 from teleop_msgs.msg import RollerTeleop
-from pynput import keyboard
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtGui import QKeyEvent
 
 
-class RollerTeleopKeyPublisher(Node):
+class RollerTeleopKeyPublisher(QWidget):
     def __init__(self):
-        super().__init__('roller_teleop_key')
-        self.publisher_ = self.create_publisher(RollerTeleop, 'roller_teleop_cmd', 10)
-        self.timer_ = self.create_timer(0.1, self.publish_commands)
+        super(RollerTeleopKeyPublisher, self).__init__()
+        self.initUI()
+        self.initROS()
+        self.timer_ = QTimer(self)
+        self.timer_.timeout.connect(self.publish_commands)
+        self.timer_.start(100)
+
+    def initUI(self):
+        self.setWindowTitle('Roller Teleop Key')
+        self.show()
+
+    def initROS(self):
+        rclpy.init(args=None)
+        self.node = Node('roller_teleop_key')
+        self.publisher_ = self.node.create_publisher(
+            RollerTeleop,
+            'roller_teleop_cmd',
+            10)
+        # self.timer_ = self.node.create_timer(0.1, self.publish_commands)
         self.command_ = [0, 0, 0]
         self.MAX_POWER = 20
-        self.listener = keyboard.Listener(
-            on_press=self.on_press,
-            on_release=self.on_release
-        )
-        self.listener.start()
-        self.get_logger().warn('Roller_teleop_key node started')
+        rclpy.spin_once(self.node, timeout_sec=1)
 
-    def on_press(self, key):
-        self.get_logger().info(f'Key {key} pressed')
-        if key == keyboard.Key.up:
+    def keyPressEvent(self, e: QKeyEvent) -> None:
+        # self.node.get_logger().info(f'{e.key()} key pressed')
+        if e.key() == Qt.Key.Key_Up:
             self.command_[2] = self.MAX_POWER
-        elif key == keyboard.Key.down:
+        elif e.key() == Qt.Key.Key_Down:
             self.command_[2] = -self.MAX_POWER
-        if key == keyboard.Key.left:
+        elif e.key() == Qt.Key.Key_Left:
             self.command_[0] = self.MAX_POWER
-            self.command_[1] = 0
-        elif key == keyboard.Key.right:
-            self.command_[0] = 0
+        elif e.key() == Qt.Key.Key_Right:
             self.command_[1] = self.MAX_POWER
+        return super().keyPressEvent(e)
 
-    def on_release(self, key):
-        self.get_logger().info(f'Key {key} released')
-        self.command_[0] = 0
-        self.command_[1] = 0
-        self.command_[2] = 0
+    def keyReleaseEvent(self, a0: QKeyEvent) -> None:
+        self.command_ = [0, 0, 0]
+        return super().keyReleaseEvent(a0)
 
     def publish_commands(self):
         msg = RollerTeleop()
@@ -45,26 +57,23 @@ class RollerTeleopKeyPublisher(Node):
         msg.auto_spd_control = self.command_[2]
 
         self.publisher_.publish(msg)
-        self.get_logger().info(f'Publishing: {msg}')
+        # self.node.get_logger().info(f'Publishing: {msg}')
 
-
-def main(args=None):
-    rclpy.init(args=args)
-    rollerteleopkey_publisher = RollerTeleopKeyPublisher()
+def main():
+    app = QApplication(sys.argv)
     try:
-        rclpy.spin(rollerteleopkey_publisher)
-    except KeyboardInterrupt:
+        roller = RollerTeleopKeyPublisher()
+    except:
         msg = RollerTeleop()
         msg.left_duty_control = 0
         msg.right_duty_control = 0
         msg.auto_spd_control = 0
-        rollerteleopkey_publisher.publisher_.publish(msg)
-        rollerteleopkey_publisher.get_logger().info('Set all commands to zero')
-        rollerteleopkey_publisher.get_logger().info('Keyboard interrupt (SIGINT)')
-    rollerteleopkey_publisher.listener.join()
-    rollerteleopkey_publisher.destroy_node()
-    rclpy.shutdown()
+        roller.publisher_.publish(msg)
+        roller.node.get_logger().info('Set all commands to zero')
+        roller.node.get_logger().info('Keyboard interrupt (SIGINT)')
+        roller.node.destroy_node()
+        rclpy.shutdown()
+    sys.exit(app.exec_())
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
