cf00942cf0d4089f4a0faca89fe0e7509e5f63f8
Jongpil Kim <jpkim@koceti.re.kr>
2023-06-15 11:40:00 +0900

    feat: path generator를 모듈형태로 변경

--- roller_control/path_generator.py
+++ roller_control/path_generator.py
@@ -1,28 +1,23 @@
-import rclpy
-from rclpy.node import Node
-from roller_interfaces.srv import Path
+from nav_msgs.msg import Path
+from geometry_msgs.msg import PoseStamped
 
 import numpy as np
 
 
-class PathGeneratorService(Node):
+class PathGenerator():
     def __init__(self):
-        super().__init__('path_generator_service')
-        self.nodeName = self.get_name()
-        self.get_logger().info("{0} started".format(self.nodeName))
-        self.service = self.create_service(Path, 'roller_path', self.generate_path)
-
-    def generate_path(self, request, response):
-        waypoints = [
+        self.waypoints = [
             (0, 0, 0.1),
             (20, 20, 0)
         ]
-        xs = waypoints[0][0]
-        xe = waypoints[1][0]
-        ys = waypoints[0][1]
-        ye = waypoints[1][1]
-        cmd_vels = waypoints[0][2]
-        cmd_vele = waypoints[1][2]
+
+    def generate_path(self):
+        xs = self.waypoints[0][0]
+        xe = self.waypoints[1][0]
+        ys = self.waypoints[0][1]
+        ye = self.waypoints[1][1]
+        cmd_vels = self.waypoints[0][2]
+        cmd_vele = self.waypoints[1][2]
 
         dist = np.sqrt(pow(xe-xs, 2) + pow(ye-ys, 2))
         count = int(dist * 10)  # 0.1m 간격으로 목표점 인터폴레이션
@@ -32,25 +27,40 @@ class PathGeneratorService(Node):
         cmd_vel = np.linspace(cmd_vels, cmd_vels, count)
         cmd_vel[-1] = cmd_vele
 
-        response.x = map_xs
-        response.y = map_ys
-        response.theta = map_yaws
-        response.vel = cmd_vel
+        path = Path()
+        path.header.frame_id = 'world'
 
-        return response
+        for i in range(len(map_xs)):
+            pose = PoseStamped()
+            pose.pose.position.x = map_xs[i]
+            pose.pose.position.y = map_ys[i]
+            pose.pose.orientation = self.get_quaternion_from_euler(0, 0, map_yaws[i])
+            path.poses.append(pose)
+
+        return path
+
+
+    def get_quaternion_from_euler(roll, pitch, yaw):
+        """
+        Convert an Euler angle to a quaternion.
+        Input
+            :param roll: The roll (rotation around x-axis) angle in radians.
+            :param pitch: The pitch (rotation around y-axis) angle in radians.
+            :param yaw: The yaw (rotation around z-axis) angle in radians.
+        Output
+            :return qx, qy, qz, qw: The orientation in quaternion [x,y,z,w] format
+        """
+        qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+        qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
+        qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
+        qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+
+        return [qx, qy, qz, qw]
 
 
 def main(args=None):
-    rclpy.init(args=args)
-    try:
-        path_generator = PathGeneratorService()
-        rclpy.spin(path_generator)
-    except KeyboardInterrupt:
-        path_generator.get_logger().info('Keyboard interrrupt (SIGINT)')
-    finally:
-        path_generator.destroy_node()
-        rclpy.shutdown()
-
+    path_generator = PathGenerator()
+    path_generator.generate_path()
 
 if __name__ == '__main__':
     main()
