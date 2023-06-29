fdb1779fd7b1b896886fb21304ebdb545f6ad9bb
Jongpil Kim <jpkim@koceti.re.kr>
2023-06-29 11:57:20 +0900

    fix[path generator]: 드럼 헤딩 GOAL(map_yaws) 계산오류 수정
    오른손 좌표계 적용하면서 X, Y 좌표를 바꿔줌

--- roller_control/path_generator.py
+++ roller_control/path_generator.py
@@ -28,7 +28,7 @@ class PathGenerator():
         count = int(dist * 10)  # 0.1m 간격으로 목표점 인터폴레이션
         map_xs = np.linspace(xs, xe, count)
         map_ys = np.linspace(ys, ye, count)
-        map_yaws = np.arctan(np.gradient(map_xs)/np.gradient(map_ys))
+        map_yaws = np.arctan2(np.gradient(map_ys), np.gradient(map_xs))
         cmd_vel = np.linspace(cmd_vels, cmd_vels, count)
         cmd_vel[-1] = cmd_vele
 
