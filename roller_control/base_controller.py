92b778f031b3eead2c92fcab418f977d90d2a3b4
Jongpil Kim <jpkim@koceti.re.kr>
2023-07-17 13:07:11 +0900

    feat[base controller]: 조향각 관련dbc 파일 원복

--- roller_control/base_controller.py
+++ roller_control/base_controller.py
@@ -22,7 +22,7 @@ class BaseController(Node):
             './install/roller_control/share/Controller_230518.dbc')
         self.can_msg_control = self.candb_controller.get_message_by_name('CONTROLLER_COMM')
         self.candb_commandsv = cantools.db.load_file(
-            './install/roller_control/share/ToSupervisor_230619.dbc')
+            './install/roller_control/share/ToSupervisor_210430.dbc')
         self.can_msg_commandsv = self.candb_commandsv.get_message_by_name('Command_SV')
 
         qos_profile = QoSProfile(depth=10)
