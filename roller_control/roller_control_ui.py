334b77ceb102595b798480603be36717e44a6356
Jongpil Kim <jpkim@koceti.re.kr>
2023-10-12 12:24:28 +0900

    fix: pyside6 사용하면서 다이얼로그 안뜨는 현상 수정

--- roller_control/roller_control_ui.py
+++ roller_control/roller_control_ui.py
@@ -9,9 +9,9 @@ import sys
 
 from ament_index_python import get_package_share_directory
 from geometry_msgs.msg import Twist
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
