683baff9bb45da17f812a1239202e196752c4129
Jongpil Kim <jpkim@koceti.re.kr>
2023-10-15 21:48:04 +0900

    feat: add surface launch file

---
+++ launch/preceeding_surface.launch.py
@@ -0,0 +1,47 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            XMLLaunchDescriptionSource(
+                [get_package_share_directory('ros2_socketcan'), '/launch/socket_can_bridge.launch.xml']
+            )
+        ),
+        Node(
+            package='gps_rclpy_pkg',
+            executable='tcpgps_pub',
+            parameters=[
+                {'gps_ip': '192.168.0.152'}
+            ]
+        ),
+        Node(
+            package='roller_control',
+            executable='roller_publisher',
+        ),
+        # 엔진 CAN 정보를 받기 위한 것 - 1세부
+        Node(
+            package='engine_can_pkg',
+            executable='get_engine_direct',
+        ),
+        # Surface 정보 생성을 위한 데이터 publish - 1,3 세부
+        Node(
+            package='makesurface_rclpy_pkg',
+            executable='surface_pub_direct',
+        ),
+        # Surface 정보 생성
+        Node(
+            package='makesurface_rclpy_pkg',
+            executable='calc_pts',
+        ),
+        # 1세부 한양대 정보 보내기
+        Node(
+            package='emulate_iotedge_pkg',
+            executable='iotemul_roller',
+        ),
+
+    ])
