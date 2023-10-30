d18122c6081adc246bbaeff75fd2c7d3e6ebe98f
Jongpil Kim <jpkim@koceti.re.kr>
2023-10-30 15:51:48 +0900

    fix: 반복하지 않는 오류 수정

--- roller_control/navigator.py
+++ roller_control/navigator.py
@@ -244,7 +244,6 @@ class Navigator(Node):
         result: MoveToPosition.Result = future.result().result
         if result.result:
             self.get_logger().info(f'Motion succeeded')
-            self.planning_index = 0
             try:
                 self.sm.navigation_done()
             except TransitionNotAllowed as e:
